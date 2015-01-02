2dab1968376ed2a7381d6f5621f3c4f3294b790c
Jacob Sayles <jsayles@gmail.com>
2015-01-02 13:02:40 -0800

    tighten up the settings

--- wedding/settings.py
+++ wedding/settings.py
@@ -2,21 +2,17 @@
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+SECRET_KEY = 'generate-a-new-one-and-put-it-in-your-local-settings-file'
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6b)4n&7b-xr^a-%ds5=n*g5vi+z#(i&f2ei!3kk$3p35ic!jeo'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
 TEMPLATE_DEBUG = False
-
 ALLOWED_HOSTS = []
 
 SITE_TITLE = "We Are Getting Married!"
+
+ROOT_URLCONF = 'wedding.urls'
+
+WSGI_APPLICATION = 'wedding.wsgi.application'
 
 INSTALLED_APPS = (
 	'django.contrib.admin',
@@ -42,19 +38,14 @@ MIDDLEWARE_CLASSES = (
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ( 'static', )
-#STATICFILES_FINDERS = (
-#	'django.contrib.staticfiles.finders.FileSystemFinder',
-#	'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#	'django.contrib.staticfiles.finders.DefaultStorageFinder',
-#)
-
-ROOT_URLCONF = 'wedding.urls'
-
-WSGI_APPLICATION = 'wedding.wsgi.application'
+STATICFILES_FINDERS = (
+	'django.contrib.staticfiles.finders.FileSystemFinder',
+	'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+	'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.sqlite3',
@@ -64,15 +55,10 @@ DATABASES = {
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'America/Los_Angeles'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 # Import the local settings file

ab5ff70317cccd58c66bf866305e12cae4ac5128
Jacob Sayles <jsayles@gmail.com>
2015-01-18 17:01:46 -0800

    cleaning up model

--- wedding/admin.py
+++ wedding/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-from wedding.models import UserProfile, Invitation
+from wedding.models import UserProfile, InvitationGroup, Invitation
 
 # Define an inline admin descriptor for UserProfile model
 class UserProfileInline(admin.StackedInline):
@@ -18,6 +18,14 @@ class UserAdmin(UserAdmin):
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
-class InvitationAdmin(admin.ModelAdmin):
+class InvitationGroupAdmin(admin.ModelAdmin):
 	pass
+admin.site.register(InvitationGroup, InvitationGroupAdmin)
+
+class InvitationAdmin(admin.ModelAdmin):
+	model = Invitation
+	list_display=("recipient", )
+	list_filter=("groups", )
+	search_fields = ('recipient', 'email1')
+
 admin.site.register(Invitation, InvitationAdmin)
--- wedding/models.py
+++ wedding/models.py
@@ -13,14 +13,31 @@ def create_user_profile(sender, instance, created, **kwargs):
 		UserProfile.objects.create(user=instance)
 post_save.connect(create_user_profile, sender=User)
 
+class InvitationGroup(models.Model):
+	name = models.CharField(max_length=32)
+
+	def __unicode__(self):
+		return (self.name)
+
 class Invitation(models.Model):
 	recipient = models.CharField("Recipient", max_length = 64)
-	address_line1 = models.CharField("Address line 1", max_length = 64)
+	address_line1 = models.CharField("Address line 1", max_length = 64, blank = True)
 	address_line2 = models.CharField("Address line 2", max_length = 64, blank = True)
 	city = models.CharField("City", max_length = 64, blank = True)
 	state = models.CharField("State", choices=STATE_CHOICES, max_length=2, blank = True)
 	zip_code = models.CharField("Zip Code", max_length = 10, blank=True)
 	email1 = models.EmailField("Primary Email", blank=True, null=True)
 	email2 = models.EmailField("Alternate Email", blank=True, null=True)
-	guest_count = models.IntegerField("Guest Count", default=0)
-	
+	rsvp_ceremony = models.PositiveSmallIntegerField("Confirmed Ceremony", blank=True, null=True)
+	rsvp_reception = models.PositiveSmallIntegerField("Confirmed Reception", blank=True, null=True)
+	rsvp_wv = models.PositiveSmallIntegerField("Confirmed West Virginia Reception", blank=True, null=True)
+	estimated_ceremony = models.PositiveSmallIntegerField("Estimated Ceremony", default=0)
+	estimated_reception = models.PositiveSmallIntegerField("Estimated Reception", default=0)
+	estimated_wv = models.PositiveSmallIntegerField("Estimated West Virginia Reception", default=0)
+	gift = models.CharField("Gift", max_length = 64, blank = True)
+	thank_you_sent = models.BooleanField(default=False)
+	notes = models.TextField(blank=True, null=True)
+	groups = models.ManyToManyField(InvitationGroup, blank=True)
+
+	def __unicode__(self):
+		return (self.recipient)

d8bf75ecb1feee7b2338c394733203aa44673e3f
Jacob Sayles <jsayles@gmail.com>
2015-02-05 08:48:38 -0800

    back to yes and no

--- wedding/admin.py
+++ wedding/admin.py
@@ -30,21 +30,21 @@ class AddressFilter(admin.SimpleListFilter):
 
 	def lookups(self, request, model_admin):
 		return (
-			('True', 'True'),
-			('False', 'False'),
+			('Yes', 'Yes'),
+			('No', 'No'),
 		)
 
 	def queryset(self, request, queryset):
-		if self.value() == 'True':
+		if self.value() == 'Yes':
 			return queryset.exclude(address_line1__exact='').exclude(city__exact='').exclude(state__exact='').exclude(zip_code__exact='')
-		if self.value() == 'False':
+		if self.value() == 'No':
 			return queryset.filter(Q(address_line1="") | Q(city="") | Q(state="") | Q(zip_code=""))
 
 class InvitationAdmin(admin.ModelAdmin):
 	def address(self):
 		if self.address_line1 and self.city and self.state and self.zip_code:
-			return True
-		return False;
+			return "Yes"
+		return "No";
 	
 	def estimated(self):
 		return self.estimated_ceremony
